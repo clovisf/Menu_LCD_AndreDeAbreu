07e6ce5a6336366b267f2e727c332f7d7a8220df
clovisf <clovisf@gmail.com>
2016-03-19 16:17:43 -0300

    primeiro commit do projeto de Menu com LCD
    primeiro commit do projeto de Menu com LCD para monitoramento de dados
    de land Rover

---
+++ menu_LandRover_r00_19032016/menu_LandRover_r00_19032016.ino
@@ -0,0 +1,161 @@
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+int botaoIncrementa = 6;
+int contador = 0;
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  tela0();
+  pinMode(botaoIncrementa, INPUT);
+ 
+  
+}
+
+void loop() {
+
+ if ((digitalRead(botaoIncrementa) == HIGH) && contador == 0) {
+  
+      tela1(); 
+      contador++;
+      delay(100);
+      
+  } else if ((digitalRead(botaoIncrementa) == HIGH) && contador == 1){
+          
+      tela2();
+      contador++;
+      delay(100);
+      
+    } else if ((digitalRead(botaoIncrementa) == HIGH) && contador == 2){
+          
+      tela3();
+      contador++;
+      delay(100);
+      
+    } else if ((digitalRead(botaoIncrementa) == HIGH) && contador == 3){
+          
+      tela4();
+      contador++;
+      delay(100);
+      
+    } else if ((digitalRead(botaoIncrementa) == HIGH) && contador == 4){
+          
+      tela5();
+      contador=0;
+      delay(100);
+      
+    } 
+    else {
+      
+    }
+
+   
+    
+ delay(50);
+  
+}
+
+void tela0(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Aperte botao p/");
+  lcd.setCursor(0, 1); 
+  lcd.setCursor(0, 1); 
+  lcd.print("mudar de tela");
+  
+
+}
+void tela1(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tela 1");
+  lcd.setCursor(0, 1); 
+  lcd.print(millis() / 1000);
+  lcd.setCursor(6, 1); 
+  lcd.print("V0= ");
+  
+
+}
+void tela2(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tela 2");
+  lcd.setCursor(0, 1); 
+  lcd.print("V1= ");
+  lcd.setCursor(6, 1); 
+  lcd.print("V2= ");
+    
+}
+void tela3(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tela 3");
+  lcd.setCursor(0, 1); 
+  lcd.print("V3= ");
+  lcd.setCursor(6, 1); 
+  lcd.print("V4= ");
+    
+}
+void tela4(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tela 4");
+  lcd.setCursor(0, 1); 
+  lcd.print("V5= ");
+  lcd.setCursor(6, 1); 
+  lcd.print("V6= ");
+    
+}
+void tela5(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Tela 5");
+  lcd.setCursor(0, 1); 
+  lcd.print("V7= ");
+  lcd.setCursor(6, 1); 
+  lcd.print("V8= ");
+    
+}
+
